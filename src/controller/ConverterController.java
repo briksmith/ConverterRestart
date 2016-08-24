08a0237c7a1c7b6521cf1d09c14d14e91b28d122
Your Name <briksmith@earthlink.net>
2016-08-24 11:19:01 -0500

    Made a result class and controller.
    Basic skelleton of MVC.  Have a controller class and soon to be a result
    class to pass around the information.
    
    Bare bones of a JSP.  Updated pom file to get necessary spring
    dependencies.
    
    Added welcome file to web.xml.

--- src/controller/ConverterController.java
+++ src/controller/ConverterController.java
@@ -1,6 +1,14 @@
 package controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class ConverterController
 {
 
+@RequestMapping("/ConverterRestart")
+public void test(){
+	System.out.println("test");
+}
+	
+	
 }
